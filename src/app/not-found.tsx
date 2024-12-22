1d0a44b41e67c9b35ffb012144dc881f2e0ca6cd
Dannick010 <danmaggot2@gmail.com>
2024-12-22 15:36:16 -0300

    feat: updateinfoPage adn createSEO

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,29 +1,12 @@
-import React from 'react'
-import { MdError } from "react-icons/md";
-import womanbarin from "/public/womanbarin.svg"
-import Image from 'next/image';
+import React from "react";
+import erro from "/public/error404.svg";
+import Image from "next/image";
 const custom404 = () => {
   return (
-    <section className='h-screen flex flex-col lg:flex-row items-center justify-center gap-10'>
-        <aside className='flex flex-col justify-center items-center'>
-              <div className='flex gap-6 items-center justify-center'>
-                <span className='text-6xl text-[#BF2C0B] '>
-                <MdError/>
-                </span>
-                <h1 className='text-5xl md:text-[64px] text-[#BF2C0B] font-bold'>404 ERROR</h1>
-              </div>
-              <h2 className='text-2xl md:text-[24px] text-[#034C8C]'>Página não existe</h2>
-        </aside>
-        <div className="relative w-[60vh] h-[60vh]">
-          <Image
-            src={womanbarin}
-            alt="woman"
-            fill
-            style={{ objectFit: "cover" }}
-          />
-        </div>
+    <section className="h-screen flex flex-col lg:flex-row items-center justify-center gap-10">
+      <Image src={erro} alt="error404 message" width={500} height={480}  />
     </section>
-  )
-}
+  );
+};
 
-export default custom404
+export default custom404;
