1226c566650b66f63ee9a4d4251f71b76528263e
Dannick010 <danmaggot2@gmail.com>
2025-03-25 17:24:29 -0300

    fix/adsense

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,20 +1,19 @@
-import type React from "react"
-import type { Metadata } from "next"
-import { Geist, Geist_Mono } from "next/font/google"
-import "./globals.css"
-import Header from "@/components/Header"
-import Footer from "@/components/Footer"
-import GoogleAdsenseScript from "@/components/GoogleAdsenseScript"
+import type React from "react";
+import type { Metadata } from "next";
+import { Geist, Geist_Mono } from "next/font/google";
+import "./globals.css";
+import Header from "@/components/Header";
+import Footer from "@/components/Footer";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
   subsets: ["latin"],
-})
+});
 
 const geistMono = Geist_Mono({
   variable: "--font-geist-mono",
   subsets: ["latin"],
-})
+});
 
 export const metadata: Metadata = {
   title: "YKMiniURL - Encurtador de Links Gratuito e Seguro",
@@ -55,28 +54,33 @@ export const metadata: Metadata = {
   other: {
     "google-adsense-account": "ca-pub-4567665986142588",
   },
-}
+};
 
 export const viewport = {
   width: "device-width",
   initialScale: 1,
   themeColor: "#ffffff",
-}
+};
 
 export default function RootLayout({
   children,
 }: Readonly<{
-  children: React.ReactNode
+  children: React.ReactNode;
 }>) {
   return (
     <html lang="pt-br">
+      <head>
+        <script
+          async
+          src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-4567665986142588"
+          crossOrigin="anonymous"
+        ></script>
+      </head>
       <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
-        <GoogleAdsenseScript />
         <Header />
         <main className="py-8 min-h-80">{children}</main>
         <Footer />
       </body>
     </html>
-  )
+  );
 }
-
